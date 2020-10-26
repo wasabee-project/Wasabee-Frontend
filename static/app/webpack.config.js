8ce35e897c658493a4c7cac1d134327869a56cc4
Scot Bontrager <scot@indievisible.org>
2020-10-25 22:41:25 -0500

    update webpack, but it breaks eslint....

--- static/app/webpack.config.js
+++ static/app/webpack.config.js
@@ -1,16 +1,19 @@
 const path = require("path");
-const ESLintPlugin = require("eslint-webpack-plugin");
+// const ESLintPlugin = require("eslint-webpack-plugin");
+const webpack = require("webpack");
+// const PrettierPlugin = require("prettier-webpack-plugin");
 
 module.exports = {
   entry: "./src/wasabee.js",
-  mode: "development",
+  // mode: "development",
+   mode: "production",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new ESLintPlugin({
-      fix: true,
-    }),
+    new webpack.ProgressPlugin(),
+    // new PrettierPlugin(),
+    // new ESLintPlugin({ fix: true })
   ],
 };

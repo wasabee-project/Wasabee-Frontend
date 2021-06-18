2f727adadcc8d43aeaf26e3beb5d0a1b1eb3f0cb
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-06-18 15:37:28 +0200

    add operation map

--- static/app/src/views/OperationMap.vue
+++ static/app/src/views/OperationMap.vue
@@ -1,1 +1,262 @@
-<template><div></div></template>
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <h1 id="opName">{{ operation.name }}</h1>
+        <LMap
+          id="map"
+          style="height: calc(100vh - 300px); min-height: 200px"
+          :bounds="operation.mbr"
+        >
+          <LControlLayers
+            position="topright"
+            :hideSingleBase="true"
+          ></LControlLayers>
+          <LTileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+            layer-type="base"
+          />
+          <LLayerGroup name="Assignments" layer-type="overlay">
+            <LMarker
+              v-for="marker in assignments.markers"
+              :key="marker.ID"
+              :lat-lng="getPortal(marker.portalId).latLng"
+              :title="getPortal(marker.portalId).name"
+            >
+              <LIcon
+                :icon-url="marker.icon"
+                :icon-size="[24, 40]"
+                :icon-anchor="[12, 40]"
+                :popup-anchor="[-1, -48]"
+              />
+              <LPopup>
+                {{ getPortal(marker.portalId).name }}
+                <div v-if="marker.comment">{{ marker.comment }}</div>
+                <div v-if="marker.status != 'pending'">{{ marker.status }}</div>
+                <div v-if="marker.assignedTo">
+                  {{ getAgentName(marker.assignedTo) }}
+                </div>
+                <b-button
+                  target="_blank"
+                  size="sm"
+                  variant="outline-primary"
+                  :href="
+                    'https://www.google.com/maps/search/?api=1&query=' +
+                    getPortal(marker.portalId).latLng.lat +
+                    ',' +
+                    getPortal(marker.portalId).latLng.lng
+                  "
+                  >Google Map</b-button
+                >
+              </LPopup>
+            </LMarker>
+            <!-- needs geodesic -->
+            <LPolyline
+              v-for="link in assignments.links"
+              :key="link.ID"
+              :lat-lngs="link.getLatLngs(operation)"
+              :weight="2"
+              :color="link.color == 'main' ? operation.color : link.color"
+            />
+            <LMarker
+              v-for="anchor in assignments.anchors"
+              :key="anchor"
+              :lat-lng="getPortal(anchor).latLng"
+              :title="getPortal(anchor).name"
+            >
+              <LIcon
+                :icon-url="cdn + '/img/markers/pin_lime.svg'"
+                :icon-size="[24, 40]"
+                :icon-anchor="[12, 40]"
+                :popup-anchor="[-1, -48]"
+              />
+              <LPopup>
+                {{ getPortal(anchor).name }}
+                <div v-if="getPortal(anchor).comment">
+                  {{ getPortal(anchor).comment }}
+                </div>
+                <div v-if="getPortal(anchor).hardness">
+                  {{ getPortal(anchor).status }}
+                </div>
+                <b-button
+                  target="_blank"
+                  size="sm"
+                  variant="outline-primary"
+                  :href="
+                    'https://www.google.com/maps/search/?api=1&query=' +
+                    getPortal(anchor).latLng.lat +
+                    ',' +
+                    getPortal(anchor).latLng.lng
+                  "
+                  >Google Map</b-button
+                >
+              </LPopup>
+            </LMarker>
+          </LLayerGroup>
+
+          <LLayerGroup name="Other" layer-type="overlay">
+            <LMarker
+              v-for="marker in others.markers"
+              :key="marker.ID"
+              :lat-lng="getPortal(marker.portalId).latLng"
+              :title="getPortal(marker.portalId).name"
+            >
+              <LIcon
+                :icon-url="marker.icon"
+                :icon-size="[24, 40]"
+                :icon-anchor="[12, 40]"
+                :popup-anchor="[-1, -48]"
+              />
+              <LPopup>
+                {{ getPortal(marker.portalId).name }}
+                <div v-if="marker.comment">{{ marker.comment }}</div>
+                <div v-if="marker.status != 'pending'">{{ marker.status }}</div>
+                <div v-if="marker.assignedTo">
+                  {{ getAgentName(marker.assignedTo) }}
+                </div>
+                <b-button
+                  target="_blank"
+                  size="sm"
+                  variant="outline-primary"
+                  :href="
+                    'https://www.google.com/maps/search/?api=1&query=' +
+                    getPortal(marker.portalId).latLng.lat +
+                    ',' +
+                    getPortal(marker.portalId).latLng.lng
+                  "
+                  >Google Map</b-button
+                >
+              </LPopup>
+            </LMarker>
+            <!-- needs geodesic -->
+            <LPolyline
+              v-for="link in others.links"
+              :key="link.ID"
+              :lat-lngs="link.getLatLngs(operation)"
+              :weight="2"
+              :color="link.color == 'main' ? operation.color : link.color"
+            />
+            <LMarker
+              v-for="anchor in others.anchors"
+              :key="anchor"
+              :lat-lng="getPortal(anchor).latLng"
+              :title="getPortal(anchor).name"
+            >
+              <LIcon
+                :icon-url="cdn + '/img/markers/pin_lime.svg'"
+                :icon-size="[24, 40]"
+                :icon-anchor="[12, 40]"
+                :popup-anchor="[-1, -48]"
+              />
+              <LPopup>
+                {{ getPortal(anchor).name }}
+                <div v-if="getPortal(anchor).comment">
+                  {{ getPortal(anchor).comment }}
+                </div>
+                <div v-if="getPortal(anchor).hardness">
+                  {{ getPortal(anchor).status }}
+                </div>
+                <b-button
+                  target="_blank"
+                  size="sm"
+                  variant="outline-primary"
+                  :href="
+                    'https://www.google.com/maps/search/?api=1&query=' +
+                    getPortal(anchor).latLng.lat +
+                    ',' +
+                    getPortal(anchor).latLng.lng
+                  "
+                  >Google Map</b-button
+                >
+              </LPopup>
+            </LMarker>
+          </LLayerGroup>
+        </LMap>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  LMap,
+  LTileLayer,
+  LControlLayers,
+  LLayerGroup,
+  LMarker,
+  LIcon,
+  LPopup,
+  LPolygon,
+  LPolyline,
+} from "vue2-leaflet";
+
+import WasabeeMe from "../me";
+
+export default {
+  props: ["operation"],
+  data: () => ({
+    me: WasabeeMe.cacheGet(),
+    cdn: window.wasabeewebui.cdnurl,
+  }),
+  computed: {
+    assignments: function () {
+      const markers = this.operation.markers.filter(
+        (m) => m.assignedTo == this.me.GoogleID
+      );
+      const links = this.operation.links.filter(
+        (m) => m.assignedTo == this.me.GoogleID
+      );
+      const anchors = new Set();
+      for (const link of links) {
+        anchors.add(link.fromPortalId);
+        anchors.add(link.toPortalId);
+      }
+      return {
+        markers,
+        links,
+        anchors,
+      };
+    },
+    others: function () {
+      const markers = this.operation.markers.filter(
+        (m) => m.assignedTo != this.me.GoogleID
+      );
+      const links = this.operation.links.filter(
+        (m) => m.assignedTo != this.me.GoogleID
+      );
+      const anchors = new Set();
+      for (const link of links) {
+        anchors.add(link.fromPortalId);
+        anchors.add(link.toPortalId);
+      }
+      return {
+        markers,
+        links,
+        anchors,
+      };
+    },
+  },
+  methods: {
+    getPortal: function (id) {
+      return this.operation.getPortal(id);
+    },
+    getAgentName: function (id) {
+      const agent = this.operation.getAgent(id);
+      if (agent) return agent.name;
+      return id;
+    },
+  },
+  components: {
+    LMap,
+    LTileLayer,
+    LControlLayers,
+    LLayerGroup,
+    LMarker,
+    LIcon,
+    LPopup,
+    // LPolygon,
+    LPolyline,
+  },
+};
+</script>
--- static/app/src/views/TeamAgentMap.vue
+++ static/app/src/views/TeamAgentMap.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col">
         <h1 id="teamName">{{ team.name }}</h1>
-        <LMap id="map" style="height: 100vh">
+        <LMap ref="map" style="height: calc(100vh - 300px); min-height: 200px">
           <LTileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
@@ -33,6 +33,13 @@ import { LMap, LTileLayer, LMarker, LIcon, LPopup } from "vue2-leaflet";
 
 export default {
   props: ["team"],
+  mounted: function () {
+    this.$refs.map.mapObject.fitBounds(
+      this.team.agents.map((a) => {
+        return [a.lat, a.lng];
+      })
+    );
+  },
   components: {
     LMap,
     LTileLayer,

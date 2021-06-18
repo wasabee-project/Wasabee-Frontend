1301cae9dc54ce70df22766dfde5d6fe938f26d9
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-06-18 12:20:46 +0200

    add team.map

--- static/app/src/views/TeamAgentMap.vue
+++ static/app/src/views/TeamAgentMap.vue
@@ -1,7 +1,35 @@
-<template></template>
+<template>
+<div class="container-fluid"><div class="row"><div class="col">
+	<h1 id="teamName">{{ team.name }}</h1>
+	<LMap id="map" style="height: 100vh">
+		<LTileLayer
+			url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+		/>
+		<LMarker v-for="agent in team.agents" :lat-lng="agent" :title="agent.name" :key="agent.id">
+			<LIcon
+				:icon-url="agent.pic"
+				:icon-size="[41, 41]"
+				:icon-anchor="[25, 41]"
+				:popup-anchor="[-1, -48]"
+			/>
+			<LPopup :content="agent.name" />
+		</LMarker>
+	</LMap>
+</div></div></div>
+</template>
 
 <script>
+import { LMap, LTileLayer, LMarker, LIcon, LPopup } from 'vue2-leaflet';
+
 export default {
-  props: ['me', 'team'],
+  props: ['team'],
+  components: {
+  	LMap,
+  	LTileLayer,
+  	LMarker,
+  	LIcon,
+  	LPopup
+  }
 }
 </script>
